d2fc76b1cb8af3632058fb97a700c30d48ba3e3e
Joao <joaog.silva152@gmail.com>
2026-06-13 05:18:32 -0300

    Responsividade finalizada e implementada funcionalidade de sugestao de receita

--- JavaScript/ReceitaAleatoria.js
+++ JavaScript/ReceitaAleatoria.js
@@ -1,18 +1,13 @@
-const BASE =
-    window.location.hostname === "joaoguimatta.github.io"
-        ? "/Panela-Quente"
-        : "";
-
 const receitas = [
-    `${BASE}/Receitas/BoloDeCenoura.html`,
-    `${BASE}/Receitas/Feijoada.html`,
-    `${BASE}/Receitas/Lasanha.html`,
-    `${BASE}/Receitas/Omelete.html`,
-    `${BASE}/Receitas/PaoDeQueijo.html`,
-    `${BASE}/Receitas/Ratattouille.html`,
-    `${BASE}/Receitas/Sanduiche.html`,
-    `${BASE}/Receitas/Strudel.html`,
-    `${BASE}/Receitas/Tapioca.html`
+    "/Panela-Quente/Receitas/BoloDeCenoura.html",
+    "/Panela-Quente/Receitas/Feijoada.html",
+    "/Panela-Quente/Receitas/Lasanha.html",
+    "/Panela-Quente/Receitas/Omelete.html",
+    "/Panela-Quente/Receitas/PaoDeQueijo.html",
+    "/Panela-Quente/Receitas/Ratattouille.html",
+    "/Panela-Quente/Receitas/Sanduiche.html",
+    "/Panela-Quente/Receitas/Strudel.html",
+    "/Panela-Quente/Receitas/Tapioca.html"
 ];
 
 function ReceitaAleatoria() {
